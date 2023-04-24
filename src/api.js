57a8ff3393dbcb571f5d86d5ec97401575850a9f
Ulugbek121 <yoqubjanov1991@gmail.com>
2023-04-24 22:52:14 +0500

    setting api key

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: "AIzaSyCcDuNYlhyVnYz0dafsmhq7r9Fx01Wpgf4",
+    key: "AIzaSyCT97gHYJedgGYOHDGuXm6YLr82QxcgdSQ",
   },
 });
 
